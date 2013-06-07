be5c6b0561944017e8c407a9c5cc2ec7492a7c24
Matias Thomsen <thomsen@200grad.de>
2013-06-07 17:07:44 +0200

    message 1

---
+++ ClassOne.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ClassOne
+ *
+ * @author Matias Thomsen
+ */
+class ClassOne {
+    //put your code here
+}
+
+?>
